f07320761b1d808fdada875511973f71561d176e
Marcos Vinicius Brito <marcosvcbrito@gmail.com>
2019-10-17 11:10:51 -0300

    Init project

---
+++ NotoriousHunt/NotoriousHunt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NotoriousHunt
+//
+//  Created by Marcos Vinicius da Cunha Brito on 17/10/19.
+//  Copyright © 2019 Marcos Vinicius da Cunha Brito. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
